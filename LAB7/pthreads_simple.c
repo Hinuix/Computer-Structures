bd90260e31d41b5e35e76314f0d7af6ec120757e
Milton Hinnant <miltonhinnant@me.com>
2022-04-07 00:31:41 -0400

    Add Solution for pthreads_simple

---
+++ LAB7/pthreads_simple.c
@@ -0,0 +1,38 @@
+#define NTHREADS 2
+#ifndef INCCNT
+#define INCCNT 1000
+#endif
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+
+void *concat(void *arg)
+{
+    char *temp = (char *)arg;
+    printf("Concat String: %s Example\n", temp);
+    return NULL;
+}
+
+void *count(void *arg)
+{
+    int *temp = (int *)arg;
+    printf("Count: %d\n", *temp + INCCNT);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t tid[NTHREADS];
+    pthread_create(&tid[0], NULL, concat, (void *)argv[1]);
+    int two = atoi(argv[2]);
+    pthread_create(&tid[1], NULL, count, (void *)&two);
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    exit(EXIT_SUCCESS);
+
+    if (argc != 3)
+    {
+        exit(EXIT_FAILURE);
+    }
+}
