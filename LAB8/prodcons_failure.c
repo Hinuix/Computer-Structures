ea70c05017889c3b196517d1c8f553fbcf661145
Milton Hinnant <miltonhinnant@me.com>
2022-04-17 20:21:52 -0400

    Add Solution for LAB8

---
+++ LAB8/prodcons_failure.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int myID;
+
+void *print_message_function(void *arg)
+{
+    int *num = (int *)arg;
+    myID = *num;
+    int i;
+    for (i = 0; i < *num; i++)
+    {
+        myID = rand() % *num;
+        printf("writer wrote: %d\n", myID);
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int read = atoi(argv[1]);
+    pthread_t threadID[1];
+    pthread_create(&threadID[0], NULL, print_message_function, (void *)&read);
+    for (i = 0; i < read; i++)
+    {
+        printf("main read: %d\n", myID);
+    }
+    pthread_join(threadID[0], NULL);
+    if (argc != 2)
+    {
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
