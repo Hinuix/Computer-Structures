1cc00bf0a52c5724145af3ee88f9218c01b688f3
Milton Hinnant <miltonhinnant@me.com>
2022-04-20 19:24:10 -0400

    part 1

--- LAB8/prodcons_mutex.c
+++ LAB8/prodcons_mutex.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 int myID;
+
+pthread_mutex_t r_mutex, w_mutex;
 
 void *print_message_function(void *arg)
 {
@@ -11,11 +14,12 @@ void *print_message_function(void *arg)
     int i;
     for (i = 0; i < *num; i++)
     {
+        pthread_mutex_lock(&w_mutex);
         myID = rand() % *num;
         printf("writer wrote: %d\n", myID);
+        pthread_mutex_unlock(&r_mutex);
     }
     return NULL;
-    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
@@ -23,15 +27,24 @@ int main(int argc, char *argv[])
     int i;
     int read = atoi(argv[1]);
     pthread_t threadID[1];
+    pthread_mutex_init(&w_mutex, NULL);
+    pthread_mutex_init(&r_mutex, NULL);
     pthread_create(&threadID[0], NULL, print_message_function, (void *)&read);
-    for (i = 0; i < read; i++)
+    if (read <= 0)
     {
-        printf("main read: %d\n", myID);
+        exit(EXIT_FAILURE);
     }
-    pthread_join(threadID[0], NULL);
     if (argc != 2)
     {
         exit(EXIT_FAILURE);
     }
+    sleep(1);
+    for (i = 0; i < read; i++)
+    {
+        pthread_mutex_lock(&r_mutex);
+        printf("main read: %d\n", myID);
+        pthread_mutex_unlock(&w_mutex);
+    }
+    pthread_join(threadID[0], NULL);
     exit(EXIT_SUCCESS);
 }
