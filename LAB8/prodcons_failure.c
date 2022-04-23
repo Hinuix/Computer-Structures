14ad43f5604a2164af694ac330f4611a5bdd3375
Milton Hinnant <miltonhinnant@me.com>
2022-04-22 22:27:16 -0400

    updating

--- LAB8/prodcons_failure.c
+++ LAB8/prodcons_failure.c
@@ -24,13 +24,7 @@ int main(int argc, char *argv[])
     int read = atoi(argv[1]);
     pthread_t threadID[1];
     pthread_create(&threadID[0], NULL, print_message_function, (void *)&read);
-
-    for (i = 0; i < read; i++)
-    {
-        printf("main read: %d\n", myID);
-    }
-    pthread_join(threadID[0], NULL);
-
+    
     if (argc != 2)
     {
         exit(EXIT_FAILURE);
@@ -38,6 +32,14 @@ int main(int argc, char *argv[])
     if (read < 1) {
       exit(EXIT_FAILURE);
     }
+
+    for (i = 0; i < read; i++)
+    {
+        printf("main read: %d\n", myID);
+    }
+    pthread_join(threadID[0], NULL);
+
+
 
     exit(EXIT_SUCCESS);
 }
