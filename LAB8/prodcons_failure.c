2fed9ddef15df9edb49da582d001293ad71ec3f0
Milton Hinnant <miltonhinnant@me.com>
2022-04-22 23:08:31 -0400

    lines added

--- LAB8/prodcons_failure.c
+++ LAB8/prodcons_failure.c
@@ -17,18 +17,27 @@ void *print_message_function(void *arg)
     return NULL;
     exit(EXIT_SUCCESS);
 }
-
 int main(int argc, char *argv[])
 {
     int i;
     int read = atoi(argv[1]);
     pthread_t threadID[1];
     pthread_create(&threadID[0], NULL, print_message_function, (void *)&read);
-    if (argc != 2){exit(EXIT_FAILURE);}
-    if (read < 1) {exit(EXIT_FAILURE); }
+        if (argc != 2)
+    {
+        exit(EXIT_FAILURE);
+    }
+    if (read < 1) {
+      exit(EXIT_FAILURE);
+    }
     for (i = 0; i < read; i++)
-    {printf("main read: %d\n", myID);}
+    {
+        printf("main read: %d\n", myID);
+    }
     pthread_join(threadID[0], NULL);
-    if (argc != 2){exit(EXIT_FAILURE);}
+    if (argc != 2)
+    {
+        exit(EXIT_FAILURE);
+    }
     exit(EXIT_SUCCESS);
 }
